a40811a9ef6a6193eb0330d02d460dc5c935c543
Matias Martin <matiasmartin84@gmail.com>
2022-01-07 23:26:17 -0300

    Comentario

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -11,3 +11,27 @@ def curso (request):
     documentoDeTexto = f"--->Curso: {curso.nombre}   Camada: {curso.camada}"
 
     return HttpResponse (documentoDeTexto)
+
+
+
+def inicio(request):
+
+    return render(request,"AppCoder/inicio.html")
+
+def cursos(request):
+
+    return render(request,"AppCoder/cursos.html")
+
+def profesores(request):
+
+    return render(request,"AppCoder/profesores.html")
+
+
+def estudiantes(request):
+
+    return render(request,"AppCoder/estudiantes.html")
+
+
+def entregables(request):
+    
+    return render(request,"AppCoder/entregables.html")
--- ProyectoCoder/urls.py
+++ ProyectoCoder/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from AppCoder.views import curso
+
 # from ProyectoCoder.AppCoder.views import curso
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("crea-curso/", curso),
+    path("AppCoder/", include("AppCoder.urls"))
+
 ]
